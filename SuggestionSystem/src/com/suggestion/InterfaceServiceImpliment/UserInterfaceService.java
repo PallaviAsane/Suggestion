ba363406d68bdcd7797c0e2aab01eee1835f7078
jayshreevshinde70 <jayshreevshinde70@gmail.com>
2017-10-31 02:41:31 -0700

    Add Comments in UserInterfaceServices

--- SuggestionSystem/src/com/suggestion/InterfaceServiceImpliment/UserInterfaceService.java
+++ SuggestionSystem/src/com/suggestion/InterfaceServiceImpliment/UserInterfaceService.java
@@ -5,44 +5,67 @@ import java.util.ArrayList;
 import com.suggestion.dao.UserDao;
 import com.suggestion.daointerface.UserInterface;
 import com.suggestion.model.User;
-
+/**
+ *  UserInterfaceService implements UserInterface to fetch UserDao class
+ * @author User
+ *
+ */
 public class UserInterfaceService implements UserInterface{
 
+	/**
+	 * addUser method calls UserDao to add user
+	 * @return boolean result
+	 */
 	@Override
 	public boolean addUser(User user) {
 		// TODO Auto-generated method stub
 		boolean adduser=UserDao.addUser(user);
 		return adduser;
 	}
-
+	/**
+	 * getUser method calls UserDao to get user
+	 * @return user
+	 */
 	@Override
 	public User getUser(int userid) {
 		// TODO Auto-generated method stub
 		User user=UserDao.getUser(userid);
 		return user;
 	}
-
+	/**
+	 * getUser method calls UserDao to gets all user
+	 * @return list of users
+	 */
 	@Override
 	public ArrayList<User> getUser(User user) {
 		// TODO Auto-generated method stub
 		ArrayList<User> listuser=UserDao.getUser(user);
 		return listuser;
 	}
-
+	/**
+	 * deleteUser method calls UserDao to delete user 
+	 * @return users
+	 */
 	@Override
 	public int deleteUser(int userid) {
 		// TODO Auto-generated method stub
 		int delete=UserDao.deleteUser(userid);
 		return delete;
 	}
-
+	/**
+	 * updateUser method calls UserDao to update user 
+	 * @return users
+	 */
 	@Override
 	public boolean updateUser(User user) {
 		// TODO Auto-generated method stub
 		boolean update=UserDao.updateUser(user);
 		return update;
 	}
-
+	/**
+	 * getUser method calls UserDao to get user 
+	 * @return user
+	 */
 	@Override
 	public int getUser(String username, String password) {
 		// TODO Auto-generated method stub
