ea9a42a072dee0c124c80caa3ed40a3c258a12fb
jayshreevshinde70 <jayshreevshinde70@gmail.com>
2017-10-31 02:39:28 -0700

    Add Comment to PdfResourceInterfaceService

--- SuggestionSystem/src/com/suggestion/InterfaceServiceImpliment/PdfResourceInterfaceService.java
+++ SuggestionSystem/src/com/suggestion/InterfaceServiceImpliment/PdfResourceInterfaceService.java
@@ -10,28 +10,40 @@ import com.suggestion.model.PdfResource;
 
 
 public class PdfResourceInterfaceService implements PdfResourceInterface{
-
+	/**
+	 * addPdfResource method calls PdfResourceDao's addPdfResource method to add PDF
+	 * @return boolean result
+	 */
 	@Override
 	public boolean addPdfResource(PdfResource pdfresource) {
 		// TODO Auto-generated method stub
 		boolean addppt=PdfResourceDao.addPdfResource(pdfresource);
 		return addppt;
 	}
-
+	/**
+	 * updatePdfResource method calls PdfResourceDao's updatePdfResource method to update PDF
+	 * @return boolean result
+	 */
 	@Override
 	public boolean updatePdfResource(PdfResource pdfresurce) {
 		// TODO Auto-generated method stub
 		boolean updatepdf=PdfResourceDao.updatePdfResource(pdfresurce);
 		return updatepdf;
 	}
-
+	/**
+	 * getAllPdfResource method calls PdfResourceDao's getAllPdfResource method to get list of PDFs
+	 * @return ArrayList<PdfResource> as a result
+	 */	
 	@Override
 	public ArrayList<PdfResource> getAllPptResource(PdfResource pptresource) {
 		// TODO Auto-generated method stub
 		ArrayList<PdfResource> pdflist=PdfResourceDao.getAllPdfResource(pptresource);
 		return pdflist;
 	}
-
+	/**
+	 * getPdfResource method calls PdfResourceDao's getPdfResource method to get single PDF
+	 * @return PdfResource as a result
+	 */
 	@Override
 	public PdfResource getPdfResource(int pdfid) {
 		// TODO Auto-generated method stub
